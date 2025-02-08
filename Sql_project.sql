79022a7602cad050e6acd9267094ea5f942d4a8e
Mukesh Kumar <102470215+Mukesh39@users.noreply.github.com>
2025-02-08 11:54:02 +0530

    Update Sql_project.sql

--- Sql_project.sql
+++ Sql_project.sql
@@ -64,7 +64,7 @@ WHERE sale_date = '2022-11-05';
 
 
 -- 2. **Write a SQL query to retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 4 in the month of Nov-2022**:
--- ```sql
+-- 
 
 SELECT 
   *
@@ -78,7 +78,7 @@ WHERE
 
 
 -- 3. **Write a SQL query to calculate the total sales (total_sale) for each category.**:
--- ```sql
+-- 
 
 
 SELECT 
@@ -90,7 +90,7 @@ GROUP BY 1
 
 
 -- 4. **Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.**:
--- ```sql
+
 
 SELECT
     ROUND(AVG(age), 2) as avg_age
@@ -99,13 +99,12 @@ WHERE category = 'Beauty'
 
 
 -- 5. **Write a SQL query to find all transactions where the total_sale is greater than 1000.**:
--- ```sql
+
 
 SELECT * FROM retail_sales
 WHERE total_sale > 1000
 
 -- 6. **Write a SQL query to find the total number of transactions (transaction_id) made by each gender in each category.**:
--- ```sql
 
 SELECT 
     category,
@@ -119,7 +118,6 @@ GROUP
 ORDER BY 1
 
 -- 7. **Write a SQL query to calculate the average sale for each month. Find out best selling month in each year**:
--- ```sql
 
 
 SELECT 
@@ -141,7 +139,6 @@ WHERE rank = 1
 
 
 -- 8. **Write a SQL query to find the top 5 customers based on the highest total sales **:
--- ```sql
 
 
 SELECT * FROM retail_sales ;
@@ -158,7 +155,6 @@ LIMIT 5 ;
 
 
 -- 9. **Write a SQL query to find the number of unique customers who purchased items from each category.**:
--- ```sql
 
 SELECT * FROM retail_sales ;
 
@@ -170,7 +166,6 @@ GROUP BY category
 
 -- -- 10. **Write a SQL query to create each shift and number of orders
 -- (Example Morning <12, Afternoon Between 12 & 17, Evening >17)**:
--- -- ```sql
 
 -- SELECT EXTRACT( HOUR FROM CURRENT_TIME) ; 
 
